01eabace4881454cfb7b55d5afaecb85f7d4cc71
A_D <aunderscored@gmail.com>
2021-05-21 04:32:52 +0200

    Added more string methods

--- util/strings.go
+++ util/strings.go
@@ -1,6 +1,9 @@
 package util
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
 
 // ReplaceAllSlice uses strings.ReplaceAll on each index of the given slice. The slice IS modified, but also returned
 func ReplaceAllSlice(in []string, old, new string) []string {
@@ -9,4 +12,23 @@ func ReplaceAllSlice(in []string, old, new string) []string {
 	}
 
 	return in
+}
+
+func ContainsAny(in string, substrs ...string) bool {
+	for _, substr := range substrs {
+		if strings.Contains(in, substr) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func MustAtoi(s string) int {
+	res, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+
+	return res
 }
